17558dcd017e2b9bd954eb947abb0ed690cf17f0
N!no <ninomyemail@gmail.com>
2023-08-01 16:06:23 -0400

    get rho transition

--- loader.py
+++ loader.py
@@ -1,64 +1,29 @@
 import numpy as np
 
 
-class DataLoader:
-    def __init__(self):
-        # X is of shape (N,F,T)
-        # where N is the space dim (node number), F is the feature dim,
-        # T is the time dim
-        self.X = None
-
-        # A is the adjacency matrix, of shape (N,N)
-        self.A = None
-
-    def train_test_split(self, dt=1, ratio=0.7):
-        T = self.X.shape[-1]
-        train_set = self.X[:, :, : int(T * ratio)]
-        test_set = self.X[:, :, int(T * ratio) :]
-
-        X_train, y_train, X_test, y_test = list(), list(), list(), list()
-        for i in range(train_set.shape[-1] - dt):
-            X = train_set[:, :, i : i + dt]
-            y = train_set[:, :, i + dt : i + 2 * dt]
-            X_train.append(X[np.newaxis, :, :, :])
-            y_train.append(y[np.newaxis, :, :, :])
-
-        X_train = np.concatenate(X_train, axis=0)
-        y_train = np.concatenate(y_train, axis=0)
-
-        for i in range(test_set.shape[-1] - dt):
-            X = test_set[:, :, i : i + dt]
-            y = test_set[:, :, i + dt : i + 2 * dt]
-            X_test.append(X[np.newaxis, :, :, :])
-            y_test.append(y[np.newaxis, :, :, :])
-
-        X_test = np.concatenate(X_test, axis=0)
-        y_test = np.concatenate(y_test, axis=0)
-
-        return (
-            np.squeeze(X_train),
-            np.squeeze(y_train),
-            np.squeeze(X_test),
-            np.squeeze(y_test),
-        )
-
-
-class RingRoadLoader(DataLoader):
-    def __init__(self, rho, V):
-        # rho and u are of shape (N, T)
-        # rho or may have one T dim
+class RingRoadLoader:
+    def __init__(self, rho, u, V):
         super().__init__()
 
-        rho = rho[:, np.newaxis, :]
-        V = V[:, np.newaxis, :]
+        self.rho, self.u, self.V = rho, u, V
+        self.N, self.T = rho.shape
+        self.init_rho, self.terminal_V = rho[:, 0], V[:, -1]
+        self.delta_x, self.delta_t = 1 / self.N, 1 / self.T
 
-        # X is of shape (N,F,T), where F=2
-        self.X = np.concatenate((rho, V), axis=1)
-
-        # For a ringroad, the diagonal and off-diagonal elements are 1, and 0 elsewhere.
-        N = self.X.shape[0]
-        a = np.zeros((1, N))[0]
-        b = np.ones((1, N - 1))[0]
+        a = np.zeros((1, self.N))[0]
+        b = np.ones((1, self.N - 1))[0]
         self.A = np.diag(a, 0) + np.diag(b, -1) + np.diag(b, 1)
         self.A[0, -1] = 1
         self.A[-1, 0] = 1
+
+    def get_transition_matrix(self):
+        transitions = list()
+        for t in range(self.T):
+            P = np.zeros((self.N, self.N), dtype=np.float32)
+            for i in range(self.N):
+                P[i, i - 1] = (self.delta_t / self.delta_x) * self.u[i - 1, t - 1]
+                P[i, i] = (self.delta_t / self.delta_x) * (1 - self.u[i, t - 1])
+
+            transitions.append(P)
+
+        return transitions
--- main.py
+++ main.py
@@ -18,83 +18,26 @@ if __name__ == "__main__":
     else:
         DEVICE = torch.device("cpu")
 
-    rho = np.loadtxt("data/rho.csv", delimiter=",")
-    V = np.loadtxt("data/V.csv", delimiter=",")[:8, :8]
-    ring_data_loader = RingRoadLoader(rho, V)
-    X_train, y_train, X_test, y_test = ring_data_loader.train_test_split(ratio=0.7)
-
-    f_x_args = (INPUT_DIM, OUTPUT_DIM, N_HIDDEN, HIDDEN_DIM)
-    f_x_kwargs = {
-        "activation_type": "none",
-        "last_activation_type": "none",
-        "device": DEVICE,
-    }
-    x = np.hstack([rho.reshape(-1, 1), V.reshape(-1, 1)])
-    f_x_kwargs["mean"] = np.array([x.mean(0)[0], 0], dtype=np.float32)
-    f_x_kwargs["std"] = np.array([x.std(0)[0], 1], dtype=np.float32)
-
-    f_m_args = (INPUT_DIM, 1, N_HIDDEN, HIDDEN_DIM)
-    f_m_kwargs = {
-        "activation_type": "none",
-        "last_activation_type": "none",
-        "device": DEVICE,
-        "mean": np.array([x.mean(0)[0], x.mean(0)[1]], dtype=np.float32),
-        "std": np.array([x.std(0)[0], x.std(0)[1]], dtype=np.float32),
-    }
-
-    mpnn = MPNN(f_x_args, f_x_kwargs, f_m_args, f_m_kwargs, ring_data_loader.A)
-    optimizer_kwargs = {"lr": 0.001}
-    optimizer = torch.optim.Adam(
-        [p for p in mpnn.parameters() if p.requires_grad is True], **optimizer_kwargs
+    rho_label = np.loadtxt("data/rho.csv", delimiter=",")
+    u_label = np.loadtxt("data/u.csv", delimiter=",")
+    V_label = np.loadtxt("data/V.csv", delimiter=",")
+    ring_data_loader = RingRoadLoader(rho_label, u_label, V_label)
+    rho_inits = np.repeat(
+        (ring_data_loader.init_rho[:, None]), ring_data_loader.T, axis=1
     )
-    loss_fun = torch.nn.MSELoss()
-
-    # train
-    best_mse, best_it, best_pred, best_val = 100000, 0, None, None
-    for it in range(2000):
-        pred = torch.squeeze(mpnn(X_train))
-        loss = loss_fun(pred, torch.Tensor(y_train))
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        val = torch.squeeze(mpnn(X_test))
-        loss_val = float(loss_fun(val, torch.Tensor(y_test)))
-
-        print(
-            "it=",
-            it,
-            "loss_train=",
-            float(loss),
-            " loss_val=",
-            loss_val,
-        )
-        if loss_val < best_mse:
-            best_mse, best_it = loss_val, it
-            best_pred, best_val = pred, val
-        if it - best_it > early_stop:
-            break
-
-    # results
-    out = np.concatenate(
-        (
-            X_train[:1, :, :],
-            best_pred.cpu().detach().numpy(),
-            X_test[:1, :, :],
-            best_val.cpu().detach().numpy(),
-        ),
-        axis=0,
-    )
-    label = np.concatenate(
-        (
-            X_train[:1, :, :],
-            y_train,
-            X_test[:1, :, :],
-            y_test,
-        ),
-        axis=0,
+    V_terminals = np.repeat(
+        (ring_data_loader.terminal_V[:, None]), ring_data_loader.T + 1, axis=1
     )
 
-    plot_3d(8, 8, out[:, :, 0], "pre")
-    plot_3d(8, 8, label[:, :, 0], "pre")
-    plot_3d(8, 8, out[:, :, 1], "pre")
-    plot_3d(8, 8, label[:, :, 1], "pre")
+    rho = np.zeros(rho_label.shape, dtype=np.float32)
+    transitions = ring_data_loader.get_transition_matrix()
+    rho[:, 0] = ring_data_loader.init_rho
+    prev_rho_t = rho[:, 0]
+    # rho[:, 1] = ring_data_loader.rho[:, 1]
+    # prev_rho_t = rho[:, 1]
+    for t in range(1, ring_data_loader.T):
+        curr_rho_t = np.dot(transitions[t - 1], prev_rho_t)
+        rho[:, t] = curr_rho_t
+        prev_rho_t = curr_rho_t
+
+    plot_3d(8, 8, rho, "pre")
