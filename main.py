778dc0320dd49ef335bbceeafc551bc8f06fa521
N!no <ninomyemail@gmail.com>
2023-08-03 17:54:24 -0400

    add src

--- main.py
+++ main.py
@@ -2,41 +2,15 @@ import yaml
 import numpy as np
 import scipy.io
 import torch
-from loader import RingRoadLoader
-from model import PIGN
-from loss import supervised_loss, transition_loss
-from utils import plot_3d, get_args_kwargs
-from test import all_transition_tester
+from src.loader import RingRoadLoader
+from src.model import PIGN
+from src.loss import supervised_loss, transition_loss
+from src.utils import plot_3d, get_args_kwargs
+from src.test import all_transition_tester
 
 
-if __name__ == "__main__":
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    with open("config.ymal", "r") as stream:
-        config = yaml.load(stream, yaml.FullLoader)
-
-    mat_file_path = config["data"]["file_path"]
-    sample_num = config["data"]["sample_num"]
-    check_id = config["data"]["check_id"]
-
-    """Loader"""
-    rho_labels = scipy.io.loadmat(mat_file_path)["rhos"]
-    u_labels = scipy.io.loadmat(mat_file_path)["us"]
-    V_labels = scipy.io.loadmat(mat_file_path)["Vs"]
-    rho_labels = np.array(rho_labels, dtype=np.float32)[:sample_num, :, :]
-    u_labels = np.array(u_labels, dtype=np.float32)[:sample_num, :, :]
-    V_labels = np.array(V_labels, dtype=np.float32)[:sample_num, :, :]
-    ring_loader = RingRoadLoader(rho_labels, u_labels, V_labels)
-
-    """Hyper-params"""
-    f_channel_args, f_channel_kwargs = get_args_kwargs(
-        config["model"]["f_channel"], device
-    )
-    f_sum_args, f_sum_kwargs = get_args_kwargs(config["model"]["f_sum"], device)
-    f_x_args, f_x_kwargs = get_args_kwargs(config["model"]["f_x"], device)
-
-    model = PIGN(
-        f_channel_args, f_channel_kwargs, f_sum_args, f_sum_kwargs, f_x_args, f_x_kwargs
-    )
+def runner(ring_loader, args, config, test=True):
+    model = PIGN(*args)
     optimizer_kwargs = {"lr": config["train"]["lr"]}
     optimizer = torch.optim.Adam(
         [p for p in model.parameters() if p.requires_grad is True], **optimizer_kwargs
@@ -49,7 +23,10 @@ if __name__ == "__main__":
 
     """Params"""
     all_transitions, all_cum_transitions = ring_loader.get_transition_matrix()
-    all_transition_tester(ring_loader, all_transitions, all_cum_transitions, check_id)
+    if test:
+        all_transition_tester(
+            ring_loader, all_transitions, all_cum_transitions, check_id
+        )
 
     init_rho_copies = np.repeat(
         (ring_loader.init_rhos[:, :, None]), ring_loader.T, axis=-1
@@ -83,4 +60,41 @@ if __name__ == "__main__":
         optimizer.step()
         print("it=", it, "loss=", float(loss))
 
-    plot_3d(8, 8, preds[check_id].detach().numpy(), r"$\rho$")
+    plot_3d(8, 8, ring_loader.rhos[check_id], f"truth-{check_id}")
+    plot_3d(8, 8, preds[check_id].detach().numpy(), f"pred-{check_id}")
+
+
+if __name__ == "__main__":
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    with open("config.ymal", "r") as stream:
+        config = yaml.load(stream, yaml.FullLoader)
+
+    mat_file_path = config["data"]["file_path"]
+    sample_num = config["data"]["sample_num"]
+    check_id = config["data"]["check_id"]
+
+    """Loader"""
+    rho_labels = scipy.io.loadmat(mat_file_path)["rhos"]
+    u_labels = scipy.io.loadmat(mat_file_path)["us"]
+    V_labels = scipy.io.loadmat(mat_file_path)["Vs"]
+    rho_labels = np.array(rho_labels, dtype=np.float32)[:sample_num, :, :]
+    u_labels = np.array(u_labels, dtype=np.float32)[:sample_num, :, :]
+    V_labels = np.array(V_labels, dtype=np.float32)[:sample_num, :, :]
+    ring_loader = RingRoadLoader(rho_labels, u_labels, V_labels)
+
+    """Hyper-params"""
+    f_channel_args, f_channel_kwargs = get_args_kwargs(
+        config["model"]["f_channel"], device
+    )
+    f_sum_args, f_sum_kwargs = get_args_kwargs(config["model"]["f_sum"], device)
+    f_x_args, f_x_kwargs = get_args_kwargs(config["model"]["f_x"], device)
+    args = (
+        f_channel_args,
+        f_channel_kwargs,
+        f_sum_args,
+        f_sum_kwargs,
+        f_x_args,
+        f_x_kwargs,
+    )
+
+    runner(ring_loader, args, config, test=True)
--- src/test.py
+++ src/test.py
@@ -1,12 +1,10 @@
 import numpy as np
-from loader import RingRoadLoader
-from utils import plot_3d
+from .utils import plot_3d
 
 
 def all_transition_tester(
-    ring_loader, all_transitions, all_cum_transitions, wanna_check=0
+    ring_loader, all_transitions, all_cum_transitions, check_id=0
 ):
-    plot_3d(8, 8, ring_loader.rhos[wanna_check], "pre")
     rhos_1 = np.zeros(
         (ring_loader.n_samples, ring_loader.N, ring_loader.T), dtype=np.float32
     )
@@ -17,7 +15,7 @@ def all_transition_tester(
                 ring_loader.init_rhos[sample_i, :],
             )
 
-    plot_3d(8, 8, rhos_1[wanna_check], "pre")
+    plot_3d(8, 8, rhos_1[check_id], "cum-trans")
 
     rhos_2 = np.zeros(
         (ring_loader.n_samples, ring_loader.N, ring_loader.T), dtype=np.float32
@@ -29,4 +27,4 @@ def all_transition_tester(
             rhos_2[sample_i, :, t] = np.dot(all_transitions[sample_i][t], prev_rho_t)
             prev_rho_t = rhos_2[sample_i, :, t]
 
-    plot_3d(8, 8, rhos_2[wanna_check], "pre")
+    plot_3d(8, 8, rhos_2[check_id], "trans")
